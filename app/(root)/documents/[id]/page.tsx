7e8aa612c52a821be2e4f40785975181e277f2d0
Khoi Nguyen <khoinguyen1942001@gmail.com>
2024-09-07 22:44:38 +0700

    Stop this project, get error by LiveBlocks

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import CollaborativeRoom from "@/components/CollaborativeRoom";
 import { getDocument } from "@/lib/actions/room.actions";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -20,10 +21,30 @@ const Document = async ({ params: { id } }: SearchParamProps) => {
   }
 
   // TODO: Assess the permissions of the user to access the document
+  const userIds = Object.keys(room?.usersAccesses || {});
+  const users = await getClerkUsers({ userIds });
+
+  const userData = users.map((user: User) => ({
+    ...user,
+    userType: room.usersAccesses[user.email]?.includes("room:write")
+      ? "editor"
+      : "viewer",
+  }));
+
+  const currentUserType = room.usersAccesses[
+    clerkUser.emailAddresses[0].emailAddress
+  ]?.includes("room:write")
+    ? "editor"
+    : "viewer";
 
   return (
     <main className="flex w-full flex-col items-center">
-      <CollaborativeRoom roomId={id} roomMetadata={room.metadata} />
+      <CollaborativeRoom
+        roomId={id}
+        roomMetadata={room.metadata}
+        users={userData}
+        currentUserType={currentUserType}
+      />
     </main>
   );
 };
